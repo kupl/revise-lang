7aaf0168a08a0985b04061eee44715b0bc3f2b44
Henry Lee <gbvrcx@gmail.com>
2022-10-28 05:48:05 +0000

    feat: verbose edit

--- bin/main.ml
+++ bin/main.ml
@@ -4,14 +4,34 @@ module Source = Interpreter.Source
 let main () =
   let pgm = ref "" in
   let target = ref "" in
-  let options = ["-target", Arg.String (fun x -> target := x), "target to rescue"] in
+  let opt_verbose = ref false in
+  let options =
+    [
+      "-target", Arg.String (fun x -> target := x), "target to rescue";
+      "-verbose", Arg.Set opt_verbose, "show edit step by step";
+    ]
+  in
   let usage = "Usage: rescue -target <target.ml> RESCUE" in
   let _ = Arg.parse options (fun x -> pgm := x) usage in
   let pgm = Parser.parse !pgm in
-  if Sys.file_exists !target then (
-    let target = Util.File.read_file_from_path !target |> Source.from_string in
-    let target = Interpreter.interprete pgm target in
-    Source.pp Format.std_formatter target;
-    Format.fprintf Format.std_formatter "\n")
+  let target =
+    (if Sys.file_exists !target then Util.File.read_file_from_path !target else !target)
+    |> Source.from_string
+  in
+  let processed =
+    if !opt_verbose then
+      List.fold_left
+        (fun src cmd ->
+          Source.pp Format.std_formatter src;
+          Format.fprintf Format.std_formatter "\n\n";
+          Format.pp_print_flush Format.std_formatter ();
+          Unix.sleepf 0.1;
+          Interpreter.interprete [cmd] src)
+        target
+        pgm
+    else Interpreter.interprete pgm target
+  in
+  Source.pp Format.std_formatter processed;
+  Format.fprintf Format.std_formatter "\n"
 
 let () = main ()
