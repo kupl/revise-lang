cab3c8c83c2205603b82dd66e3459cbbe6b0780c
Henry Lee <gbvrcx@gmail.com>
2022-10-28 14:33:56 +0000

    feat: inplacement option

--- bin/main.ml
+++ bin/main.ml
@@ -2,24 +2,35 @@ module Program = Ast.Lang.Program
 module Source = Interpreter.Source
 
 let main () =
+  (* Args *)
   let pgm = ref "" in
-  let target = ref "" in
+  let opt_target = ref "" in
   let opt_verbose = ref false in
+  let opt_inplace = ref false in
   let options =
     [
-      "-target", Arg.String (fun x -> target := x), "Target to rescue";
+      "-target", Arg.String (fun x -> opt_target := x), "Target to rescue";
       "-verbose", Arg.Set opt_verbose, "Show edit step by step";
+      "-inplace", Arg.Set opt_inplace, "Inplace the update";
     ]
   in
-  let usage = "Usage: rescue -target <target.ml> RESCUE" in
+  let usage = "Usage: rescue [-verbose] [-inplace] -target <target.ml> RESCUE" in
   let _ = Arg.parse options (fun x -> pgm := x) usage in
+  let _ =
+    if (not (Sys.file_exists !opt_target)) && !opt_inplace then (
+      print_endline "-target must be a file with -inplace option";
+      exit 1)
+  in
+  (* Rescue *)
   let pgm =
     (if Sys.file_exists !pgm then Util.File.read_file_from_path !pgm else !pgm) |> Parser.parse
   in
+  (* Source *)
   let target =
-    (if Sys.file_exists !target then Util.File.read_file_from_path !target else !target)
+    (if Sys.file_exists !opt_target then Util.File.read_file_from_path !opt_target else !opt_target)
     |> Source.from_string
   in
+  (* Interprete *)
   let processed =
     if !opt_verbose then (
       Format.fprintf Format.std_formatter "\n\n\n\n\n\n\n\n\n\n\n\027[11F";
@@ -45,7 +56,14 @@ let main () =
       src)
     else Interpreter.interprete pgm target
   in
-  Source.pp ~with_cursor:false Format.std_formatter processed;
-  Format.fprintf Format.std_formatter "\n"
+  (* Print *)
+  if !opt_inplace then
+    Source.pp
+      ~with_cursor:false
+      (!opt_target |> open_out |> Format.formatter_of_out_channel)
+      processed
+  else (
+    Source.pp ~with_cursor:false Format.std_formatter processed;
+    Format.fprintf Format.std_formatter "\n")
 
 let () = main ()
